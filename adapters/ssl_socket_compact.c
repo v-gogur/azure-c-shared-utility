f4933619c914c0d92df1d81b77132b52ad4c4d8d
Roy Sprowl <roys@jetstreamsoftware.com>
2017-04-04 15:51:18 -0700

    Updated ssl_socket

--- adapters/ssl_socket_compact.c
+++ adapters/ssl_socket_compact.c
@@ -12,6 +12,8 @@
 #include <sys/socket.h>
 #endif // LINUX
 
+#include <sys.time>
+
 
 #include "azure_c_shared_utility/ssl_socket.h"
 #include "azure_c_shared_utility/xlogging.h"
@@ -25,6 +27,32 @@
 #define EXTRACT_IPV4(ptr) ((struct sockaddr_in *) ptr->ai_addr)->sin_addr.s_addr
 #endif
 
+#ifndef AZURE_SSL_TIMEOUT_SECONDS
+#define AZURE_SSL_TIMEOUT_SECONDS   20
+#endif // !AZURE_SSL_TIMEOUT_SECONDS
+
+
+///////////////////////////////////////////////////////////////////////////////
+// These socket behavior constants are chosen as good matches for Azure IoT,
+// and it should be unnecessary to to change them for most applications. 
+// However, if it is necessary, they can be altered without code change by
+// redefining the values at compile time.
+///////////////////////////////////////////////////////////////////////////////
+#ifndef AZURE_SSL_SOCKET_SO_KEEPALIVE
+#define AZURE_SSL_SOCKET_SO_KEEPALIVE 1    /* enable keepalive */
+#endif
+
+#ifndef AZURE_SSL_SOCKET_TCP_KEEPIDLE
+#define AZURE_SSL_SOCKET_TCP_KEEPIDLE 30   // wait for 30s of inactivity before starting keepalive
+#endif
+
+#ifndef AZURE_SSL_SOCKET_TCP_KEEPINTVL
+#define AZURE_SSL_SOCKET_TCP_KEEPINTVL 30  // send a keepalive packet every 30 seconds
+#endif
+
+#ifndef AZURE_SSL_SOCKET_TCP_KEEPCNT
+#define AZURE_SSL_SOCKET_TCP_KEEPCNT 3     /* retry count */
+#endif
 
 
 
@@ -78,9 +106,7 @@ static uint32_t get_ipv4(const char* hostname)
 
 int SSL_Socket_Create(const char* hostname, int port)
 {
-    int result = -1;
-    int ret;
-    int sock = -1;
+    int sock;
 
     struct sockaddr_in sock_addr;
 
@@ -92,28 +118,32 @@ int SSL_Socket_Create(const char* hostname, int port)
         if (sock < 0)
         {
             LogError("create socket failed");
+            sock = SSL_SOCKET_NULL_SOCKET;
         }
         else
         {
-            int keepAlive = 1; //enable keepalive
-            int keepIdle = 20; //20s
-            int keepInterval = 2; //2s
-            int keepCount = 3; //retry # of times
+            // sock is now good and has a non-negative value
+            int config_result;
 
-            ret = 0;
-            ret = ret || setsockopt(sock, SOL_SOCKET, SO_KEEPALIVE, (void *)&keepAlive, sizeof(keepAlive));
-            ret = ret || setsockopt(sock, IPPROTO_TCP, TCP_KEEPIDLE, (void *)&keepIdle, sizeof(keepIdle));
-            ret = ret || setsockopt(sock, IPPROTO_TCP, TCP_KEEPINTVL, (void *)&keepInterval, sizeof(keepInterval));
-            ret = ret || setsockopt(sock, IPPROTO_TCP, TCP_KEEPCNT, (void *)&keepCount, sizeof(keepCount));
+            int keepAlive = AZURE_SSL_SOCKET_SO_KEEPALIVE; // enable keepalive
+            int keepIdle = AZURE_SSL_SOCKET_TCP_KEEPIDLE; // wait for AZURE_SSL_SOCKET_TCP_KEEPIDLE seconds of inactivity before starting keepalive
+            int keepInterval = AZURE_SSL_SOCKET_TCP_KEEPINTVL; // send the keepalive packet every AZURE_SSL_SOCKET_TCP_KEEPINTVL seconds
+            int keepCount = AZURE_SSL_SOCKET_TCP_KEEPCNT; // retry AZURE_SSL_SOCKET_TCP_KEEPCNT of times before declaring the connection dead
+
+            config_result = 0;
+            config_result = config_result != 0 || setsockopt(sock, SOL_SOCKET, SO_KEEPALIVE, (void *)&keepAlive, sizeof(keepAlive));
+            config_result = config_result != 0 || setsockopt(sock, IPPROTO_TCP, TCP_KEEPIDLE, (void *)&keepIdle, sizeof(keepIdle));
+            config_result = config_result != 0 || setsockopt(sock, IPPROTO_TCP, TCP_KEEPINTVL, (void *)&keepInterval, sizeof(keepInterval));
+            config_result = config_result != 0 || setsockopt(sock, IPPROTO_TCP, TCP_KEEPCNT, (void *)&keepCount, sizeof(keepCount));
 
             // NB: On full-sized (multi-process) systems it would be necessary to use the SO_REUSEADDR option to 
             // grab the socket from any earlier (dying) invocations of the process and then deal with any 
-            // residual junk in the connection stream. This doesn't happen with embedded, so it doesn't need
+            // residual junk in the connection stream. Embedded systems don't have multiple processes, so it doesn't need
             // to be defended against.
 
-            if (ret != 0)
+            if (config_result != 0)
             {
-                LogError("set socket keep-alive failed, ret = %d ", ret);
+                LogError("set socket keep-alive config failed, config_result = %d ", config_result);
             }
             else
             {
@@ -128,9 +158,9 @@ int SSL_Socket_Create(const char* hostname, int port)
                 sock_addr.sin_addr.s_addr = 0;
                 sock_addr.sin_port = 0; // random local port
 
-                ret = bind(sock, (struct sockaddr*)&sock_addr, sizeof(sock_addr));
+                config_result = bind(sock, (struct sockaddr*)&sock_addr, sizeof(sock_addr));
 
-                if (ret)
+                if (config_result != 0)
                 {
                     LogError("bind socket failed");
                 }
@@ -142,8 +172,8 @@ int SSL_Socket_Create(const char* hostname, int port)
                     sock_addr.sin_addr.s_addr = ipV4address;
                     sock_addr.sin_port = htons(port);
 
-                    ret = connect(sock, (struct sockaddr*)&sock_addr, sizeof(sock_addr));
-                    if (ret == -1)
+                    config_result = connect(sock, (struct sockaddr*)&sock_addr, sizeof(sock_addr));
+                    if (config_result == -1)
                     {
                         int sockErr = get_socket_errno(sock);
                         if (sockErr != EINPROGRESS)
@@ -161,8 +191,10 @@ int SSL_Socket_Create(const char* hostname, int port)
                             FD_SET(sock, &writeset);
                             FD_SET(sock, &errset);
 
-                            ret = select(sock + 1, NULL, &writeset, &errset, NULL);
-                            if (ret <= 0)
+                            struct timeval timeout = { .tv_sec = AZURE_SSL_TIMEOUT_SECONDS, .tv_usec = 0 };
+
+                            config_result = select(sock + 1, NULL, &writeset, &errset, &timeout);
+                            if (config_result <= 0)
                             {
                                 LogError("Select failed: %d", get_socket_errno(sock));
                             }
@@ -192,12 +224,13 @@ int SSL_Socket_Create(const char* hostname, int port)
                     }
                 }
             }
+            // If we're not configured properly, then don't return the socket
+            if (sock != SSL_SOCKET_NULL_SOCKET && config_result != 0)
+            {
+                SSL_Socket_Close(sock);
+                sock = SSL_SOCKET_NULL_SOCKET;
+            }
         }
-    }
-
-    if (sock >= 0 && result < 0)
-    {
-        SSL_Socket_Close(sock);
     }
     return result;
 }
